99701d9a9ef0c1b2d2c57c58bb88a80d9636b460
Harry Jackson <harrystujackson@gmail.com>
2024-04-21 10:39:16 +1200

    feat: makeMove logic for Game model

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -16,9 +16,26 @@ class Game {
     this.currentPlayer = "X";
   }
 
-  makeMove() {
-    // TODO:
-    //
+  makeMove(position: number) {
+    if (!this.gameOver && !this.board.isEmptyCell(position)) {
+      this.board.placeMark(position, this.currentPlayer);
+
+      const winner = this.board.isWinner();
+      if (winner) {
+        this.gameOver = true;
+        return; // winner?
+      }
+
+      if (this.board.isBoardFull()) {
+        this.gameOver = true;
+        return; // some other state?
+      }
+
+      this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+    }
+
+    // do nothing unsuccessful
+    return false;
   }
 }
 
