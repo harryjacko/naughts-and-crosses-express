b7a1b08031c61e0d7601c57b480e24b0dc3f03e6
Harry Jackson <harrystujackson@gmail.com>
2024-04-25 15:01:21 +1200

    feat: enabling cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes";
 
@@ -7,6 +8,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
